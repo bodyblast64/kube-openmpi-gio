684d3850779133628dc4815d346ffcce42a89588
Stephen Knox <stephen.knox@manchester.ac.uk>
2021-05-17 09:43:13 +0100

    Add some more commands to the run script

--- run.sh
+++ run.sh
@@ -19,9 +19,39 @@ kubectl -n $KUBE_NAMESPACE exec -it $MPI_CLUSTER_NAME-master -- mpiexec --allow-
   exit 0
 fi
 
+if [[ $1 == 'pywr-simple' ]];
+then
+kubectl -n $KUBE_NAMESPACE exec -it $MPI_CLUSTER_NAME-master -- mpirun --allow-run-as-root \
+  --hostfile /kube-openmpi/generated/hostfile \
+  --use-hwthread-cpus \
+  --display-map -n 140 -npernode 70 \
+   python3 /app/pywr-to-borg/examples/simple_reservoir_system.py mpi-search archive.json --max-evaluations=1000
+  exit 0
+fi
+
+if [[ $1 == 'wre-test' ]];
+then
+kubectl -n $KUBE_NAMESPACE exec -it $MPI_CLUSTER_NAME-master -- mpirun --allow-run-as-root \
+  --hostfile /kube-openmpi/generated/hostfile \
+  --use-hwthread-cpus \
+  --display-map -n 2 -npernode 2 \
+   export HDF5_USE_FILE_LOCKING=FALSE; wre-moea borg-search /moea/wre/WRE_Simulator/models/wre-search-stochastic-lpm.json --max-evaluations 150000 --output-frequency 500 --random-seed 4 --suffix=test
+  exit 0
+fi
+
+if [[ $1 == 'wre-full' ]];
+then
+kubectl -n $KUBE_NAMESPACE exec -it $MPI_CLUSTER_NAME-master -- mpirun --allow-run-as-root \
+  --hostfile /kube-openmpi/generated/hostfile \
+  --use-hwthread-cpus \
+  --display-map -n 140 -npernode 70 \
+   export HDF5_USE_FILE_LOCKING=FALSE; wre-moea borg-search models/wre-search-stochastic-lpm.json --max-evaluations 150000 --output-frequency 500 --random-seed 4 --suffix=test
+  exit 0
+fi
+
 
 usage(){
-    echo "./run.sh basic or ./run.sh mpi4py"
+    echo "./run.sh basic or ./run.sh mpi4py or ./run.py pywr-simple or ./run.py wre-test or ./run.py wre-full"
 }
 
 usage
