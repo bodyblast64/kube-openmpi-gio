86940cfff4e6e67022cec8145a0c2f34dbf74e2e
Giovanni Basolu <giovanni.basolu@gmail.com>
2022-04-22 15:15:13 +0100

    new label

--- restart_cluster.sh
+++ restart_cluster.sh
@@ -11,9 +11,9 @@ do
     then
         break
     fi
-    sleep 1
+    sleep 5
 done
- 
+
 cp values.yaml values_edited.yaml
 sed -i "s/<USER>/$USER/g" values_edited.yaml
 helm template moea-mpi chart --namespace $KUBE_NAMESPACE -f values_edited.yaml -f ssh-key.yaml | kubectl -n $KUBE_NAMESPACE create -f -
